0d78a88411651f774bf96684e620457049bde715
Achuthanandan  J <achuthanandanj@gmail.com>
2025-07-16 08:05:58 +0000

    test

---
+++ models/WRK/wrk_orders_v.sql
@@ -0,0 +1,6 @@
+select o_orderkey as "order_id",
+       o_custkey as "customer_id",
+       o_orderstatus as "status",
+       o_orderdate as "order_date",
+       o_comment as "comments"
+from dbt_demo.raw.orders
--- models/WRK/wrk_sales.sql
+++
@@ -1,9 +0,0 @@
--- models/staging/stg_sales.sql
-
-SELECT
-    Store AS store_id,
-    Dept AS department_id,
-    TO_DATE(Date, 'DD/MM/YYYY') AS week_date,
-    TO_NUMBER(Weekly_Sales) AS total_weekly_sales,
-    IsHoliday AS is_holiday_week
-FROM {{ source('miniproj', 'SALES_DATA_SET') }}

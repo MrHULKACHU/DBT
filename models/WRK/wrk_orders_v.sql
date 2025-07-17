9779dd1c27687886e83304cedecde9c27031b06e
Achuthanandan  J <achuthanandanj@gmail.com>
2025-07-17 07:34:43 +0000

    dbt_demo wrk&pub model completed.

--- models/WRK/wrk_orders_v.sql
+++ models/WRK/wrk_orders_v.sql
@@ -1,11 +1,7 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
-select o_orderkey as "order_id",
-       o_custkey as "customer_id",
-       o_orderstatus as "status",
-       o_orderdate as "order_date",
-       o_comment as "comments"
-from dbt_demo.raw.orders
+SELECT 
+  o_orderkey AS order_id,
+  o_custkey AS customer_id,
+  o_orderstatus AS status,
+  o_orderdate AS order_date,
+  o_comment AS comments
+FROM {{ source('dbt_demo', 'orders') }}
